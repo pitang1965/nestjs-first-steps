1ad71b9bfa862ae2a716b77ac2449e610bac76ac
pitang1965 <pitang1965@gmail.com>
2024-07-15 16:07:27 +0900

    /publicを追加

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -9,4 +9,9 @@ export class AppController {
   getHello(): string {
     return this.appService.getHello();
   }
+
+  @Get('public')
+  getPublic(): string {
+    return this.appService.getPublic();
+  }
 }
